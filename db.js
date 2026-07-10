47d82dfabef768c0dd4bfdae7e2ceec59a4239e9
ericmullo <ericrodrigomullo@gmail.com>
2026-07-10 17:10:18 -0500

    Primera versión del juego

--- db.js
+++ db.js
@@ -2,11 +2,10 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT)
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production"
+        ? { rejectUnauthorized: false }
+        : false
 });
 
 pool.connect()
@@ -15,7 +14,7 @@ pool.connect()
         cliente.release();
     })
     .catch((error) => {
-        console.error("❌ ERROR DE CONEXIÓN:");
+        console.error("❌ Error de conexión:");
         console.error(error.message);
     });
 
